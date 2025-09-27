7932a8389c710bba94c301ddd1363b8f9ca6670a
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-09-26 22:23:11 -0300

    resolution

--- services/movie.py
+++ services/movie.py
@@ -14,10 +14,10 @@ def get_movies(
     queryset = Movie.objects.all()
 
     if genres_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids)
+        queryset = queryset.filter(genres__id__in=genres_ids).distinct()
 
     if actors_ids:
-        queryset = queryset.filter(actors__id__in=actors_ids)
+        queryset = queryset.filter(actors__id__in=actors_ids).distinct()
 
     return queryset
 
